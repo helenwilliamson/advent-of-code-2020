402ea16c60e313809f6545351fe81a3638c092c2
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2020-12-07 21:13:09 +0000

    puzzle 3 - part 2 (not working yet)

--- src/advent_of_code_2020/puzzle-3.clj
+++ src/advent_of_code_2020/puzzle-3.clj
@@ -8,11 +8,21 @@
        (str/split-lines)
        (map cycle)))
 
-(defn navigate-simple-slope
-  [slope]
-  (let [indexes (iterate (partial + 3) 0)]
+(defn navigate-slope
+  [slope right down]
+  (let [data (take-nth down slope)
+        indexes (iterate (partial + right ) 0)]
     (->> (interleave indexes slope)
          (partition 2)
          (map (fn [[i row]] (nth row i)))
          (filter (partial = \#))
          count)))
+
+(defn navigate-simple
+  [slope]
+  (navigate-slope slope 3 1))
+
+(defn navigate-complicated
+  [slope]
+  (->> [[1 1] [3 1] [5 1] [7 1] [1 2]]
+         (map #(apply (partial navigate-slope slope) %))))
