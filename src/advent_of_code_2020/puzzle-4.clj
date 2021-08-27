e150db8a6dbd584d95679235e051701a17bee0aa
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2021-08-27 21:24:33 +0100

    puzzle-4 part 2

--- src/advent_of_code_2020/puzzle-4.clj
+++ src/advent_of_code_2020/puzzle-4.clj
@@ -23,6 +23,37 @@
             (filter (partial not= "cid"))
             count)))
 
-(defn count-valid
+(defn count-mandatory
   [passports]
   (count (filter mandatory-fields? passports)))
+
+(defn hgt-checker
+  [value]
+  (let [[[text amount units]] (re-seq #"(?<value>\d+)(?<units>cm|in)" value)]
+    (when (some? text)
+      (case units
+        "cm" (<= 150 (Integer/parseInt amount) 193)
+        "in" (<= 59 (Integer/parseInt amount) 76)))))
+
+(def eye-colours #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+
+(defn valid?
+  [passport]
+  (let [byr-checker (fn [value] (and (>= value 1920) (<= value 2020)))
+        iyr-checker (fn [value] (and (>= value 2010) (<= value 2020)))
+        eyr-checker (fn [value] (and (>= value 2020) (<= value 2030)))
+        hcl-checker (fn [value] (some? (re-matches #"#[0-9a-f]{6}" value)))
+        ecl-checker (fn [value] (contains? eye-colours value))
+        pid-checker (fn [value] (some? (re-matches #"[0-9]{9}" value)))]
+    (and (mandatory-fields? passport)
+         (byr-checker (Integer/parseInt (passport "byr")))
+         (iyr-checker (Integer/parseInt (passport "iyr")))
+         (eyr-checker (Integer/parseInt (passport "eyr")))
+         (hgt-checker (passport "hgt"))
+         (hcl-checker (passport "hcl"))
+         (ecl-checker (passport "ecl"))
+         (pid-checker (passport "pid")))))
+
+(defn count-valid
+  [passports]
+  (count (filter valid? passports)))
