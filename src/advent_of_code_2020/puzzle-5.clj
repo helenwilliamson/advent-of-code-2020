71976d2c2f26ebfbd86a477baf60578221c4dd27
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2021-09-01 13:26:58 +0100

    refactored

--- src/advent_of_code_2020/puzzle-5.clj
+++ src/advent_of_code_2020/puzzle-5.clj
@@ -7,31 +7,27 @@
   (->> (slurp "resources/puzzle-5")
        (str/split-lines)))
 
-(defn row-number
-  [boarding-pass]
-  (loop [row-desigation (take 7 boarding-pass)
-         rows (range 0 128)]
-    (let [row-decider (fn [letter] (case letter
-                                   \F 0
-                                   \B 1))
-          current (first row-desigation)
+(defn looper
+  [relevant-parts upper-limit decider]
+  (loop [desigation relevant-parts
+         rows (range 0 upper-limit)]
+    (let [current (first desigation)
           partitioned (split-at (/ (count rows) 2) rows)]
       (if (nil? current)
         (first rows)
-        (recur (rest row-desigation) (partitioned (row-decider current)))))))
+        (recur (rest desigation) (partitioned (decider current)))))))
+
+(defn row-number
+  [boarding-pass]
+  (looper (take 7 boarding-pass) 128 #(case %1
+                                        \F 0
+                                        \B 1)))
 
 (defn column-number
   [boarding-pass]
-  (loop [row-desigation (drop 7 boarding-pass)
-         rows (range 0 8)]
-    (let [row-decider (fn [letter] (case letter
-                                   \L 0
-                                   \R 1))
-          current (first row-desigation)
-          partitioned (split-at (/ (count rows) 2) rows)]
-      (if (nil? current)
-        (first rows)
-        (recur (rest row-desigation) (partitioned (row-decider current)))))))
+  (looper (drop 7 boarding-pass) 8 #(case %1
+                                      \L 0
+                                      \R 1)))
 
 (defn version-1
   []
