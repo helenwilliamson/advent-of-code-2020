cd4f4d37ee066a6efde1ad2e0eeec8200b429099
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2021-09-01 19:37:35 +0100

    puzzle 6 complete

--- src/advent_of_code_2020/puzzle-6.clj
+++ src/advent_of_code_2020/puzzle-6.clj
@@ -1,19 +1,31 @@
 (ns advent-of-code-2020.puzzle-6
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as cset]))
 
 (defn read-input
   []
   (map str/split-lines (-> (slurp "resources/puzzle-6")
                            (str/split #"\n\n"))))
 
-(defn unique-answers
+(defn someone-answered
   [group-answers]
   (into #{} (flatten (map #(into [] %1) group-answers))))
+
+(defn everyone-answered
+  [group-answers]
+  (apply cset/intersection (map #(into (hash-set) %1) group-answers)))
 
 (defn version-1
   []
   (->> (read-input)
-       (map unique-answers)
+       (map someone-answered)
+       (map count)
+       (reduce +)))
+
+(defn version-2
+  []
+  (->> (read-input)
+       (map everyone-answered)
        (map count)
        (reduce +)))
