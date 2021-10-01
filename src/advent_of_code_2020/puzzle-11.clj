271c45a337ebd162c2099534a8284dba2570e964
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2021-10-01 09:14:14 +0100

    Still stuck on puzzle 11 - part 1

--- src/advent_of_code_2020/puzzle-11.clj
+++ src/advent_of_code_2020/puzzle-11.clj
@@ -10,21 +10,66 @@
                  (map #(str/split %1 #""))))))
 
 (defn seats-around
-  [y x width height]
+  [y x height width]
   (into #{})(->> (for [a (range (dec x) (+ 2 x))
                        b (range (dec y) (+ 2 y))]
-                   [a b])
-                 (filter #(and (not (and (= (first %1) x) (= (second %1) y)))
+                   [b a])
+                 (filter #(and (not (and (= (first %1) y) (= (second %1) x)))
                                (> (first %1) -1)
                                (> (second %1) -1)
                                (< (first %1) width)
                                (< (second %1) height)))))
 
+(defn all-seats
+  [width height]
+  (for [x (range width)
+        y (range height)]
+    [y x]))
+
+(def occupied-seat "#")
+(def empty-seat "L")
+
 (defn count-occupied
-  [seat-plan [x y]]
+  [seat-plan [y x]]
   (let [width (count (first seat-plan))
         height (count seat-plan)
-        seats-around-here (seats-around x y width height)]
+        seats-around-here (seats-around y x height width)]
     (->> (map #(get-in seat-plan %1) seats-around-here)
-         (filter #(= "#" %1))
+         (filter #(= occupied %1))
          (count))))
+
+(defn apply-rules
+  [seat-plan seat-location]
+  (let [seat (get-in seat-plan seat-location)
+        number-occupied (count-occupied seat-plan seat-location)]
+    (cond
+       (and (= empty-seat seat) (= 0 number-occupied)) occupied-seat
+       (and (= occupied-seat seat) (>= number-occupied 4)) empty-seat
+       :else seat)))
+
+(defn seat-people
+  [seat-plan]
+  (let [width (count (first seat-plan))
+        height (count seat-plan)
+        seats (all-seats width height)]
+    (loop [plan seat-plan]
+      (let [updated-plan (reduce (fn [current-plan seat-location]
+                                   (let [seat (get-in plan seat-location)
+                                         new-seat (apply-rules plan seat-location)]
+                                     (if (not= new-seat seat)
+                                       (assoc-in current-plan seat-location new-seat)
+                                       current-plan)))
+                                 plan
+                                 seats)]
+        (if (= plan updated-plan)
+          plan
+          (recur updated-plan))))))
+
+(defn version-1
+  ([] (version-1 "puzzle-11"))
+  ([name]
+   (->> (read-input name)
+        (seat-people)
+        (flatten)
+        (filter #(= %1 occupied))
+        (count))))
